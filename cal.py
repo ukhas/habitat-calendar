22fedf13e133ff284fb144437a1d0c0d01298710
Adam Greig <adam@adamgreig.com>
2012-04-11 00:27:14 +0100

    upgrade to proper icalendar; add info to core object closing #3, #4

--- cal.py
+++ cal.py
@@ -6,7 +6,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def calendar():
-    c = icalendar.Calendar()
+    cal = icalendar.Calendar()
+    cal.add('prodid', '-//habhub//NONSGML habitat-calendar//EN')
+    cal.add('version', '2.0')
+    cal.add('x-wr-calname', 'Upcoming HAB launches')
+
     db = couchdbkit.Server("http://habitat.habhub.org")['habitat']
     flights = db.view("calendar/flights", stale='update_after')
 
@@ -18,9 +22,9 @@ def calendar():
         e.add('dtend', datetime.datetime.fromtimestamp(launchtime + 4*3600))
         e.add('dtstamp', datetime.datetime.utcnow())
         e['uid'] = flight['id']
-        c.add_component(e)
+        cal.add_component(e)
 
-    return Response(c.as_string(), mimetype='text/calendar')
+    return Response(cal.to_ical(), mimetype='text/calendar')
 
 if __name__ == "__main__":
     app.run()
