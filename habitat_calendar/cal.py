1648a17b603a29a1512b955fa345f06763973192
Adam Greig <adam@adamgreig.com>
2012-04-11 00:56:20 +0100

    include various bits of payload config data, closes #2

--- habitat_calendar/cal.py
+++ habitat_calendar/cal.py
@@ -4,6 +4,22 @@ import icalendar
 from flask import Flask, Response
 app = Flask(__name__)
 
+def desc_from_payload(payload, config):
+    desc = "{payload}: ".format(**locals())
+    pairs = {}
+    try:
+        pairs.update(config['radio'])
+        if type(config['telemetry']) == list:
+            for telem in config['telemetry']:
+                pairs.update(telem)
+        else:
+            pairs.update(config['telemetry'])
+    except KeyError:
+        pass
+    for k, v in pairs.iteritems():
+        desc += "{k}: {v};".format(**locals())
+    return desc
+
 @app.route("/")
 def calendar():
     cal = icalendar.Calendar()
@@ -16,15 +32,26 @@ def calendar():
 
     for flight in flights:
         launchtime = flight['key']
+        desc = []
+        if 'metadata' in flight['value']:
+            desc.append("Metadata:")
+            for key, value in flight['value']['metadata'].iteritems():
+                desc.append("{key}: {value}".format(**locals()))
+        if 'payloads' in flight['value']:
+            desc.append("")
+            desc.append("Payloads:")
+            for payload, config in flight['value']['payloads'].iteritems():
+                desc.append(desc_from_payload(payload, config))
         e = icalendar.Event()
         e.add('summary', flight['value']['name'] + " Launch")
+        e.add('description', "\n".join(desc))
         e.add('dtstart', datetime.datetime.fromtimestamp(launchtime))
         e.add('dtend', datetime.datetime.fromtimestamp(launchtime + 4*3600))
         e.add('dtstamp', datetime.datetime.utcnow())
-        e['uid'] = flight['id']
+        e['uid'] = str(flight['id'])
         cal.add_component(e)
 
     return Response(cal.to_ical(), mimetype='text/calendar')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
