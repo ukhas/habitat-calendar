7cda20d2f34a343856f341574362520f4e2be0d4
Adam Greig <adam@adamgreig.com>
2012-04-11 01:14:01 +0100

    what was I thinking, don't include the venv

--- venv/lib/python2.6/site-packages/socketpool/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -
-#
-# This file is part of socketpool.
-# See the NOTICE for more information.
-
-from socketpool.pool import ConnectionPool
-from socketpool.conn import Connector, TcpConnector
